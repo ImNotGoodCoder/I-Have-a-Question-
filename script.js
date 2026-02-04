adbafbace5b61d8a1437fe309dc9a37d4e300a5b
ImNotGoodCoder <salemetienne7@gmail.com>
2026-02-04 14:12:02 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function selectOption(option) {
     if (option === 'yes') {
         // Flash rainbow colors
         flashRainbowColors(function() {
-            document.getElementById('question').style.display = 'none'; // Hide the question
+            document.getElementById('question').innerText = "That was the only correct answer 😌"; // Hide the question
             displayCatHeart(); // Display the cat-heart.gif
         });
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
-        document.getElementById('no-button').innerText = 'You sure?'; 
+        document.getElementById('no-button').innerText = 'Wait… think again 👀'; 
         // Increase font size of "Yes" button
         var yesButton = document.getElementById('yes-button');
         var currentFontSize = window.getComputedStyle(yesButton).getPropertyValue('font-size');
